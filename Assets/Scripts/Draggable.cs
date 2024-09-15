9ae2c7c601cd5fba56ef504170d3bcce8eee6d4c
BP-Feral <mihai.pricob@yahoo.com>
2024-09-15 17:08:22 +0300

    ✨ New Items
    14 Total drink types.

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -9,10 +9,14 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public Image image;
     [HideInInspector]
     public Transform parentAfterDrag;
+    private Vector3 dragOffset;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin drag");
+
+        dragOffset = transform.position - Input.mousePosition;
+
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
@@ -22,7 +26,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
+        transform.position = Input.mousePosition + dragOffset;
     }
 
     public void OnEndDrag(PointerEventData eventData)
